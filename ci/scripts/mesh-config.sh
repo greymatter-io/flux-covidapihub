b27cd9eab50417e3037eac5a612aaaf85d451522
Hiromi Suenaga <hiromis@gmail.com>
2020-04-15 17:33:17 -0500

    Updating

--- ci/scripts/mesh-config.sh
+++ ci/scripts/mesh-config.sh
@@ -4,8 +4,17 @@ greymatter version
 
 source ./ci/scripts/mesh-env.sh
 
+listener=$(lsof -t -i:10080)
+
+if [ ! -z "$listener" ]; then
+    echo "Killing a process (pid $listener) using port 10080"
+    kill $listener
+fi
+
+kubectl -n fabric wait --for=condition=Ready pod/api-0  --timeout=300s
+
 kubectl port-forward api-0 -n fabric 10080:10080 &
-sleep 5
+sleep 10
 
 echo "Starting mesh configuration ..."
 
@@ -25,7 +34,6 @@ create_or_update() {
     # If response from the api is null, try editing the object
     if [ -z "$resp" ]; then
         echo "Object already exists! Editing $file"
-	#exit 1
         greymatter edit $1 _ <$file
     fi
 
