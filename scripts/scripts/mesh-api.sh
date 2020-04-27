d096c71e4a3d1b0b1a7f7e8c27161e2be85d7f79
zoemccormick <zmm7@cornell.edu>
2020-04-27 09:31:25 -0400

    apply api mesn configs

--- scripts/scripts/mesh-api.sh
+++ scripts/scripts/mesh-api.sh
@@ -14,15 +14,43 @@ if [[ $ENV == "prod" && "$(kubectl config current-context)" = arn:aws:eks:*  ]];
     echo "🍀🍀🍀🍀🍀 Just so you know, you are changing production 🍀🍀🍀🍀🍀"
 fi
 
+listener=$(lsof -t -i:10080)
+if [ ! -z "$listener" ]; then
+    echo "Killing a process (pid $listener) using port 10080"
+    kill $listener
+fi
+
 listener=$(lsof -t -i:10081)
 if [ ! -z "$listener" ]; then
     echo "Killing a process (pid $listener) using port 10081"
     kill $listener
 fi
 
+# Create or update takes an object type and an optional filename
+create_or_update() {
+    file=$2
+    # If file is null, set to objecttype.json, eg route.json
+    if [ -z "$file" ]; then
+        file=$1.json
+    fi
+
+    echo "Creating object with $file"
+    resp=$(greymatter create $1 <$file)
+    echo "$resp"
+    # If response from the api is null, try editing the object
+    if [ -z "$resp" ]; then
+        echo "Object already exists! Editing $file"
+        greymatter edit $1 _ <$file
+    fi
+
+    echo "----------"
+}
+
+kubectl port-forward api-0 -n fabric 10080:10080 &
 kubectl port-forward deployment/catalog -n sense 10081:10080 &
 sleep 15
 
+objects="domains clusters listeners proxies rules routes"
 for meshfolder in apis/*; do
     if [ -d "$meshfolder" ]; then
         for folder in $objects; do
