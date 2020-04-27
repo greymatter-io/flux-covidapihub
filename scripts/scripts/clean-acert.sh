b15c43fcc785b5ef3194187a0d15ecc2afa84e1d
Hiromi Suenaga <hiromis@gmail.com>
2020-04-27 08:32:41 -0500

    Adding warning

--- scripts/scripts/clean-acert.sh
+++ scripts/scripts/clean-acert.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "Are you sure you want to delete your Acert files? Hit ENTER to continue; or any letters then ENTER to exit."
+read answer
+
+if [ ! -z "$answer" ]; then
+    echo "Exiting without deleting Acert files"
+    exit 1
+fi
+
+echo "Okay! Deleting Acert files."
+
 # Delete Acert files
 rm -f ~/.acert/authorities/*
 rm -f ~/.acert/leaves/*
