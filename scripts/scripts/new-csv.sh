7ac309fee258411aa92f36cedb024fdefb542d6a
Zoe McCormick <32151242+zoemccormick@users.noreply.github.com>
2020-04-28 13:48:59 -0400

    Script swagger (#238)
    * add swagger doc configmap for apier

--- scripts/scripts/new-csv.sh
+++ scripts/scripts/new-csv.sh
@@ -19,25 +19,23 @@ echo "Capability (health, governance, etc.)":
 read content_type
 echo Docs link:
 read docs
+echo Description:
+read description
+echo "Updates (ex. Daily, Monthly, 5 Minutes)":
+read updates
+echo "Coverage (ex. US)":
+read coverage
+echo Thumbnail:
+read thumbnail
+echo "Format (JSON, CSV, etc.)":
+read format
 
-read -r -p "Add details for catalog metadata? [y/N] (description, coverage, updates, etc.) " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    echo Description:
-    read description
-    echo "Updates (ex. Daily, Monthly, 5 Minutes)":
-    read updates
-    echo "Coverage (ex. US)":
-    read coverage
-    echo Thumbnail:
-    read thumbnail
-    echo "Format (JSON, CSV, etc.)":
-    read format
-else
+if [[ $format == "" ]]; then
     format="JSON"
 fi
 
-# convert sort and group-by fields to lowercase
 content_type=$(perl -e "print lc('$content_type');")
+content_type="$(tr '[:lower:]' '[:upper:]' <<< ${content_type:0:1})${content_type:1}"
 
 capability=\"\\\"{\\\\\\\"name\\\\\\\":\\\\\\\"$display_name\\\\\\\",\\\\\\\"url\\\\\\\":\\\\\\\"$csv_url\\\\\\\",\\\\\\\"description\\\\\\\":\\\\\\\"${description}\\\\\\\",\\\\\\\"source\\\\\\\":\\\\\\\"$owner\\\\\\\",\\\\\\\"contentType\\\\\\\":[\\\\\\\"$content_type\\\\\\\"],\\\\\\\"homePage\\\\\\\":\\\\\\\"$docs\\\\\\\",\\\\\\\"thumbnail\\\\\\\":\\\\\\\"$thumbnail\\\\\\\",\\\\\\\"coverage\\\\\\\":[\\\\\\\"$coverage\\\\\\\"],\\\\\\\"format\\\\\\\":[\\\\\\\"$format\\\\\\\"],\\\\\\\"updates\\\\\\\":[\\\\\\\"$updates\\\\\\\"]}\\\"\"
 
@@ -50,6 +48,7 @@ mkdir apis/$name/mesh/routes
 mkdir apis/$name/mesh/proxies
 mkdir apis/$name/mesh/rules
 scripts/resources/api_files/csv.deployment.sh $name $csv_url $sheet_name >apis/$name/$name.deployment.yaml
+scripts/resources/api_files/swagger.configmap.sh $name >apis/$name/$name.swagger.configmap.yaml
 scripts/resources/api_files/sidecar_configmap.sh $name >apis/$name/$name.sidecar.configmap.yaml
 scripts/resources/api_files/domain.csv.sh $name "0.0.0.0" >apis/$name/mesh/domains/$name.domain.ingress.json
 scripts/resources/api_files/listener.sh $name >apis/$name/mesh/listeners/$name.listener.ingress.json
@@ -61,7 +60,7 @@ scripts/resources/api_files/local.rules.sh $name >apis/$name/mesh/rules/local.ru
 scripts/resources/api_files/edge.route.sh $name >apis/$name/mesh/routes/edge.$name.route.json
 scripts/resources/api_files/local.route.sh $name "" >apis/$name/mesh/routes/local.route.json
 scripts/resources/api_files/edge.route.slash.sh $name >apis/$name/mesh/routes/edge.$name.route.slash.json
-scripts/resources/api_files/catalog.sh $name "$display_name" "$owner" "$capability" "$docs" >apis/$name/mesh/catalog.$name.json
+scripts/resources/api_files/catalog.sh $name "$display_name" "$owner" "$capability" "/apis/$name/docs/" >apis/$name/mesh/catalog.$name.json
 
 echo ""
 echo "Generating Catalog envvars, checking covidapihub for number of services"
@@ -69,7 +68,7 @@ count=$(curl -k https://covidapihub.io/catalog/latest/zones/default.zone | jq .c
 echo $count
 echo "The current service count is: $count, incrementing by 1"
 count=$((count+1))
-scripts/resources/catalog.envvars.sh $name "$display_name" "$owner" "$capability" "$docs" "$count" >apis/$name/mesh/catalog.envvars.yaml
+scripts/resources/catalog.envvars.sh $name "$display_name" "$owner" "$capability" "/apis/$name/docs/" "$count" >apis/$name/mesh/catalog.envvars.yaml
 echo ""
 echo "Copy the following envvars (theyre also stored in apis/$name/mesh/catalog.envvars.yaml) and paste them into the catalog container env"
 echo ""
