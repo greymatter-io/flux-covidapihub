fa1e6a27d0c5608f72d80d8970e6f0cff41ad1ba
Coleman McFarland <coleman.mcfarland+dontlaugh@gmail.com>
2020-06-16 10:33:58 -0400

    Add in a load test script that builds a wrk script
    It only builds the script but doesn't execute it.

---
+++ scripts/helpers/load-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+ENDPOINT="/ping"
+THREADS="5"
+CONNS="5"
+DURATION="30s"
+HOST_HEADER="disease.sh"
+HISTORY="$HOME/.rlwrap_load_test_history"
+IP="54.196.249.239"
+
+value_prompt() {
+    local prompt=$1
+    local dfault=$2
+    echo -n $(rlwrap -pYellow -S "$prompt: " -P $dfault -H "$HISTORY" -D 1 -o cat)
+}
+
+IP=$(value_prompt "cluster IP" $IP)
+ENDPOINT=$(value_prompt "endpoint" $ENDPOINT)
+THREADS=$(value_prompt "num threads" $THREADS)
+CONNS=$(value_prompt "connections" $CONNS)
+DURATION=$(value_prompt "duration" $DURATION)
+HOST_HEADER=$(value_prompt "Host header" $HOST_HEADER)
+
+echo ""
+echo "Copy the following work script and EXECUTE it (if it looks good)"
+echo "wrk -t${THREADS} -c${CONNS} -d${DURATION} -H \"Host: $HOST_HEADER\" https://${IP}${ENDPOINT}"
+
