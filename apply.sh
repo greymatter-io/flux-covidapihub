ddb3374c772b1c9b6af8020f86d206ccf84aeb9d
kaitmore <kaitmore@gmail.com>
2020-11-19 13:33:40 -0500

    sealed secrets

--- apply.sh
+++ apply.sh
@@ -6,5 +6,11 @@
 # kubectl create namespace fabric
 # for f in fabric/*.yaml; do kubectl apply -f $f; done
 
-kubectl create namespace sense
-for f in sense/*.yaml; do kubectl apply -f $f; done
+# kubectl create namespace sense
+# for f in sense/*.yaml; do kubectl apply -f $f; done
+
+# kubectl create namespace website
+# for f in website/*.yaml; do kubectl apply -f $f; done
+
+kubectl create namespace data
+for f in data/*.yaml; do kubectl apply -f $f; done
