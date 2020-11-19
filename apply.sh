dba7f31f584c4738e0e3d3dca0edba029e4fda2d
kaitmore <kaitmore@gmail.com>
2020-11-19 12:29:39 -0500

    remove index.docker.io

--- apply.sh
+++ apply.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-kubectl create namespace spire
-for f in spire/registrar.*.yaml; do kubectl apply -f $f; done
-for f in spire/server.*.yaml; do kubectl apply -f $f; done
+# kubectl create namespace spire
+# for f in spire/registrar.*.yaml; do kubectl apply -f $f; done
+# for f in spire/server.*.yaml; do kubectl apply -f $f; done
+# kubectl create namespace fabric
+# for f in fabric/*.yaml; do kubectl apply -f $f; done
+
+kubectl create namespace sense
+for f in sense/*.yaml; do kubectl apply -f $f; done
