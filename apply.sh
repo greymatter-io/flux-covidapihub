f800f6b0a67a045c72daa2393e7b6c7074f2074d
kaitmore <kaitmore@gmail.com>
2020-11-23 15:44:41 -0500

    add secret, move novelcovid out of apis

--- apply.sh
+++ apply.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# kubectl create namespace spire
-# for f in spire/registrar.*.yaml; do kubectl apply -f $f; done
-# for f in spire/server.*.yaml; do kubectl apply -f $f; done
+# cd ~/Decipher/helm-charts/spire
+# git checkout -b copy-covid && git pull
+# make spire
+
+# kubectl create namespace cert-manager
+# for f in cert-manager/*.yaml; do kubectl apply -f $f; done
+
 # kubectl create namespace fabric
 # for f in fabric/*.yaml; do kubectl apply -f $f; done
 
@@ -14,8 +18,10 @@
 
 # kubectl create namespace data
 # for f in data/*.yaml; do kubectl apply -f $f; done
-# kubectl create namespace cert-manager
-# for f in cert-manager/*.yaml; do kubectl apply -f $f; done
 
 # kubectl create namespace edge
-for f in edge/*.yaml; do kubectl apply -f $f; done
+# for f in edge/*.yaml; do kubectl apply -f $f; done
+
+kubectl create namespace apis
+for f in apis/*.yaml; do kubectl apply -f $f; done
+for f in apis/*/*.yaml; do kubectl apply -f $f; done
